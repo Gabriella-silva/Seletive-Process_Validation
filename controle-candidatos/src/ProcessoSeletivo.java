0ec420c6ee1b10f65cc47c572ccf5cb68b27408d
gabriella-silva <gabriellaferreira534@gmail.com>
2025-02-22 20:45:59 -0300

    <3

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -1,7 +1,9 @@
 
 public class ProcessoSeletivo{
     public static void main (String[] Args){
-        System.out.println("Processo Seletivo");
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
     }
     static void analisarCandidato( double salarioPrtendido){
         double salarioBase = 2000.0;
@@ -9,7 +11,10 @@ public class ProcessoSeletivo{
         System.out.println("LIGAR PARA O CANDIDATO");
         }
         else if(salarioBase == salarioPrtendido){
-            System.out.println("AGUARDANDO DEMAIS CANDIDATOS");
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
         }
+        else{
+        System.out.println("AGUARDANDO DEMAIS CANDIDATOS");
+    }
     }
 }
