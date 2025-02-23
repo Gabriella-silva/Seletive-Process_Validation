724983e6bd6f405ca73b5cfd0230dee54f0fd115
gabriella-silva <gabriellaferreira534@gmail.com>
2025-02-22 21:02:23 -0300

    <3

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -1,20 +1,36 @@
-
+//biblioteca random
+import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo{
     public static void main (String[] Args){
-        analisarCandidato(1900.0);
+        /*analisarCandidato(1900.0);
         analisarCandidato(2200.0);
         analisarCandidato(2000.0);
+    }*/}
+    static void selecaoCandidatos (){
+        String [] candidatos = {"Felipe", "Marcia", " Julia", "Paulo", "Augusto", "Monica", "Fabricio"};
     }
+
+
     static void analisarCandidato( double salarioPrtendido){
         double salarioBase = 2000.0;
-        if(salarioBase > salarioPrtendido){
+            if(salarioBase > salarioPrtendido){
         System.out.println("LIGAR PARA O CANDIDATO");
         }
-        else if(salarioBase == salarioPrtendido){
+            else if(salarioBase == salarioPrtendido){
             System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
         }
-        else{
+            else{
         System.out.println("AGUARDANDO DEMAIS CANDIDATOS");
     }
     }
+
+    /*Case 2 : Foi solicitado que nosso sistema garanta que sejamm selecionados apenas 5 candidaturas onde o salario seja menor ou igual */
+
+//biblioteca random
+static double valolorPretendido(){
+    return ThreadLocalRandom.current().nextDouble(1800, 2200);
+}
+
+
+
 }
